664cf5e24c4495a7832525b48471ba5c0c40ca3b
sadam-PK <sadamccr@gmail.com>
2024-12-08 01:06:13 +0500

    updated

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { connectDB } from "./db/connectDB.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
+import path from 'path'
 
 import authRoutes from "./routes/auth.route.js";
 
@@ -12,17 +12,18 @@ const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend/dist/index.html"));
-  });
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")))
+  app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  })
 }
 
 app.listen(PORT, () => {
